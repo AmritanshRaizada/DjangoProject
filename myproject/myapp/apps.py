56c9db88b1753a0cd28b4ee3909ce6d09c3f53d3
raj <rajsavita992@gmail.com>
2025-04-03 03:47:18 +0530

    myapp

--- myproject/myapp/apps.py
+++ myproject/myapp/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class MyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp'
+    name = ''
 
     def ready(self):
         import myapp.signals  # Ensure signals are loaded
